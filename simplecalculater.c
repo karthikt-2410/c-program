2d0b7277a2685c5c73451db0c2373abdd2dd1b1c
Karthikeyan <karthikt2410@gmail.com>
2026-07-17 19:23:40 +0530

    Add simple calculator for basic arithmetic operations

---
+++ simplecalculater.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+ {
+    int a;
+    int b;
+    printf("Enter value for A :");
+    scanf("%d",&a);
+    printf("Enter value for B :");
+    scanf("%d",&b);
+    printf("%d\nA+B=",a+b);
+    printf("%d\nA-B=",a-b);
+    printf("%d\nAxB=",a*b);
+    printf("%d\nA/B=",a/b);
+    
+    return 0;
+}
