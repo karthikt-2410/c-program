5a8a5d3534d5187933ad0964482d57772f7001a1
Karthikeyan <karthikt2410@gmail.com>
2026-07-22 21:27:46 +0530

    Add functions to find largest value and print star pattern

--- task2.c
+++ task2.c
@@ -27,3 +27,34 @@ int main()
     }
     return 0;
 }
+//3.find largest value 
+{
+     int a,b,c;
+     printf("Enter value for a:");
+     scanf("%d",&a);
+     printf("Enter value for b:");
+     scanf("%d",&b);
+     printf("Enter value for c:");
+     scanf("%d",&c);
+     if(a>b && a>c){
+        printf("The largest value od three is:%d\n",a);
+     }else if(a<b && b>c){
+        printf("The largest value od three is:%d\n",b);
+     }else{
+        printf("The largest value od three is:%d\n",c);
+     }
+}
+//4.star patten
+{
+    int n;
+    printf("Enter a number :");
+    scanf("%d",&n);
+    for(int i=1;i<=n;i++){
+            for(int j=n;j>i;j--){
+                printf("  ");
+            }
+            for(int k=1;k<=(2*i-1);k++){
+                printf("* ");
+            }
+            printf("\n");
+}
