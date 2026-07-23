7e598e329a2b29abf68f61eed303aa8c1ddab5e8
Karthikeyan <karthikt2410@gmail.com>
2026-07-23 22:07:57 +0530

    Add leap year check functionality

--- task2.c
+++ task2.c
@@ -58,3 +58,17 @@ int main()
             }
             printf("\n");
 }
+//5.leap year
+ {
+    int year;
+
+    printf("Enter year: ");
+    scanf("%d", &year);
+
+    if (year % 4 == 0){
+        printf("%d  Leap Year", year);
+    }else{
+        printf("%d Not a Leap Year", year);
+    }
+    return 0;
+}
